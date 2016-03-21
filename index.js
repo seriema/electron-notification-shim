93c4bf88a3428f58743e0a0b769a17b38727c60d
Alex Buznik <shu@buznik.net>
2016-03-21 14:48:39 +0200

    Fix deprecated dependency ipc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Electron doesn't support notifications in Windows yet. https://github.com/atom/electron/issues/262
 // So we hijack the Notification API.'
-const ipc = require('ipc');
+const ipc = require('electron').ipcRenderer;
 
 module.exports = () => {
 	const OldNotification = Notification;
