e2672dd96c64a58f0b91550675104ab4cfb35ebf
Jeff Bargmann <jeffba@yahoo-inc.com>
2016-04-27 00:30:36 -0700

    Add support for notification overrides
    Host may wish to silence notifications, or hide the body of
    notifications for privacy.

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const ipc = require('ipc');
 module.exports = () => {
 	const OldNotification = Notification;
 
+	var settings = {
+		forceSilent: false,
+		bodyOverride: undefined
+	};
+
 	Notification = function (title, options) {
 		// Send this to main thread.
 		// Catch it in your main 'app' instance with `ipc.on`.
@@ -16,6 +21,11 @@ module.exports = () => {
 			options
 		});
 
+		// Apply overrides
+		options = Object.assign({}, options);
+		if (settings.forceSilent) options.silent = true;
+		if (settings.bodyOverride) options.body = settings.bodyOverride;
+
 		// Send the native Notification.
 		// You can't catch it, that's why we're doing all of this. :)
 		return new OldNotification(title, options);
@@ -24,4 +34,6 @@ module.exports = () => {
 	Notification.prototype = OldNotification.prototype;
 	Notification.permission = OldNotification.permission;
 	Notification.requestPermission = OldNotification.requestPermission;
+
+	return settings;
 };
